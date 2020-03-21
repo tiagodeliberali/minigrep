62c51f929c7028d87d32f07f2f30ffce79acff36
Tiago Deliberali <tiago.deliberali@gmail.com>
2020-03-21 12:26:25 -0300

    Finished exercise

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::process;
+use minigrep::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let config = Config::build_from_args().unwrap_or_else(|err| {
+        eprintln!("{}", err);
+        process::exit(1);
+    });
+
+    if let Err(err) = minigrep::run(config) {
+        eprintln!("Não deu para rodar: {}", err);
+        process::exit(1);
+    }
 }
